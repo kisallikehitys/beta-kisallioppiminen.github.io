222324da32d25c5103caafcde1f6eadced2a60ff
Greatman Lim <greatman.lim@mahti.org>
2017-04-08 21:09:02 +0300

    Remove duplicated Session init call

--- js/init.js
+++ js/init.js
@@ -1,9 +1,9 @@
 var view = new View();
 const backend = new Backend();
 Session.init();
+console.log('Session.init() has been called!');
 window.onload = function() {
 
-	Session.init();
   view.showNavigation();
 
 }
