68802b66db7caebcf3aacfc2c72bfa8e877eaf9a
Greatman Lim <greatman.lim@mahti.org>
2017-05-03 19:05:23 +0300

    Add getSchedule function

--- js/scheduleManager.js
+++ js/scheduleManager.js
@@ -69,7 +69,27 @@ class ScheduleManager {
     backend.get(`courses/${courseId}/schedules`)
       .then(
           function fulfilled(data) {
+
+            view.clearScheduleManagerColor();
+
+            let colors = ['brown', 'blue', 'green', 'orange', 'yellow'];
+            let isReserved;
+
+            for (let i = 0; i < colors.length; i++) {
+              for (let j = 0; j < data.length; j++) {
+                if (data[j].color - 1 == i) {
+                  view.createScheduleColorSection(colors[i], data[j].name);
+                  isReserved = true;
+                }
+              }
+              if (!isReserved) {
+                view.createScheduleColorSection(colors[i]);
+              }
+              isReserved = false;
+            }
+
             console.log('Got schedule:' + data);
+
           },
           function rejected() {
             console.log('could not get schedule');
