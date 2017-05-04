e1cc1e711cc46f18815a33e1c81f91251b678092
Greatman Lim <greatman.lim@mahti.org>
2017-05-04 16:43:14 +0300

    Load ScheduleManager in init.js

--- js/init.js
+++ js/init.js
@@ -1,6 +1,13 @@
 const view = new View();
 const backend = new Backend();
+
 Session.init();
+
+let schedulemanager;
+if (button._isTeacherCourse()) {
+  schedulemanager = new ScheduleManager();
+}
+
 window.onload = function() {
 
   view.showNavigation();
